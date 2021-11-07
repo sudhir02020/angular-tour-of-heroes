c9e0a7b0e5699dbd5a4b53cca2f0e7d5f4d6642e
Sudhir <sudhirweb404@gmail.com>
2021-11-07 18:03:04 +0530

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES :Hero[]=[
+
+  { id: 11, name: 'Rampal Yadav' },
+  { id: 12, name: 'Ranbeer Kapoor' },
+  { id: 13, name: 'Sudhir Kumar' },
+  { id: 14, name: 'Puneeth Rajkumar' },
+  { id: 15, name: 'Ajay devgan' },
+  { id: 16, name: 'Lalu Yadav' },
+  { id: 17, name: 'Sanjay Dutt' },
+  { id: 18, name: 'Bin Desel' },
+  { id: 19, name: 'Raj Kumar' },
+  { id: 20, name: 'Raj Kumar Rao' }
+]
